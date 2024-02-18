d17bdfe4eb9386a11ddf678e581ac15008e88349
JocelynVelarde <a01285780@itesm.mx>
2024-02-18 17:50:40 -0600

    created project structure and initial pages

---
+++ home.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title('Home')
+st.divider()
+
+st.write('Welcome to the home page!')
+
+st.page_link("pages/landing.py", label="Click this button to Get Started", icon="🚀")
---
+++ pages/landing.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title('Landing')
+st.divider()
+
+st.write('Welcome to the landing page!')
