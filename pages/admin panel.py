44433d58f41e185983ec935000e1e9e84eda34f0
JocelynVelarde <a01285780@itesm.mx>
2024-02-23 08:36:58 -0600

    update admin

--- pages/admin panel.py
+++ pages/admin panel.py
@@ -1,4 +1,6 @@
 import streamlit as st
 
 st.title('Admin Panel')
+
+st.write('Welcome to the admin panel. Here you can create, delete, train and evaluate AI assistants in a simple manner, so you can keep them with up-to-date information.')
 st.divider()
