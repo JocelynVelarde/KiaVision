343cdbfc7232452fbf4e25be5e29e05705b3b40d
Jocelyn Velarde <70779495+JocelynVelarde@users.noreply.github.com>
2024-02-23 08:34:52 -0600

    Update get started.py

--- pages/get started.py
+++ pages/get started.py
@@ -3,4 +3,6 @@ import streamlit as st
 st.title('Get Started')
 st.divider()
 
-st.write('Instructions on how to get started with Kia Vision AI will be provided here.')
+st.write('1. Navigate to user panel')
+st.write("2. Type in your message on the problem you are experiencing with your Kia")
+st.write("3. Or upload a picture with the issue on your car and our trained model will tell you how to procede")
